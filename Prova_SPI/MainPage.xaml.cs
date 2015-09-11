4c6354f281811faa3d5b5da99412f6047444b4d6
Nicolas Farabegoli <nicolas.farabegoli@icloud.com>
2015-09-11 12:30:22 +0200

    Creata prova SPI

---
+++ Prova_SPI/MainPage.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Spi;
+using Windows.Devices.Gpio;
+using System.Threading.Tasks;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Prova_SPI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private SpiDevice SPI;
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            InitAll();
+        }
+
+        private async void InitAll()
+        {
+            try
+            {
+                await InitSpi();
+            }
+            catch
+            {
+                
+            }
+        }
+
+       
+        private async Task InitSpi()
+        {
+            try
+            {
+                var settingsSPI = new SpiConnectionSettings(0);
+                settingsSPI.ClockFrequency = 10000000;
+                settingsSPI.Mode = SpiMode.Mode3;
+
+                string SPIController = SpiDevice.GetDeviceSelector("SPI0");
+                var deviceInfo = await DeviceInformation.FindAllAsync(SPIController);
+                SPI = await SpiDevice.FromIdAsync(deviceInfo[0].Id, settingsSPI);
+            } 
+            catch (Exception ex)
+            {
+                throw new Exception("SPI Initialization Failed", ex);
+            }
+        }
+
+
+        private void AD9834Send(byte[] Data)
+        {
+
+        }
+
+        private void SetOffsetRange_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+
+        }
+
+        private void SetAmplitudeRange_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+
+        }
+
+        private void SetFrequencyRange_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
+        {
+
+        }
+
+        private void SetSinWave_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SetTriWave_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SetSqrWave_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FrequencyTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AmplitudeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void OffsetTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
